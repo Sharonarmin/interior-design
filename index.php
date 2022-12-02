6458639c96fe5224a4df8a7959af490444c62c86
unknown <sharonarmin84@gmail.com>
2022-12-02 12:56:52 +0530

    about section

--- index.php
+++ index.php
@@ -10,7 +10,8 @@
     <!-- slick -->
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
-    
+    <!-- animate -->
+    <!-- <link rel="stylesheet" href="assets/css/animate.min.css"> -->
     <!-- style -->
     <link rel="stylesheet" href="assets/css/style.css">
 
@@ -65,10 +66,62 @@
     <!-- about section -->
 
     <div class="about-section">
-        
+        <div class="container">
+            <div class="row">
+                <div class="row">
+                    <div class="col-md-6 col-12 p-0">
+                        <img src="assets/images/about.png" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-md-6 col-12 about-desc-sec">
+                        <div class="about-right">
+                            <h3 class="main-head">About Us</h3>
+                            <p class="main-para">Lorem ipsum dolor sit amet consectetur. Dictum justo faucibus pulvinar pulvinar purus donec id tristique.
+                             Vulputate amet arcu sed in facilisis. Lorem ipsum dolor sit amet consectetur. Dictum justo faucibus pulvinar
+                              pulvinar purus donec id tristique. Vulputate amet arcu sed in facilisis.</p>
+                              <p class="main-para">Lorem ipsum dolor sit amet consectetur. Dictum justo faucibus pulvinar pulvinar purus donec id tristique.
+                             Vulputate amet arcu sed in facilisis. Lorem ipsum dolor sit amet consectetur.</p>
+                            <div class="counter-interior mt-3">
+                                <div class="me-4">
+                                    <div class="counter-div"><span class="counter">123</span>+</div>
+                                    <div class="counter-label">Projects</div>
+                                </div>
+                                <div>
+                                    <div class="counter-div"><span class="counter">600</span>+</div>
+                                    <div class="counter-label">Clients</div>
+                                </div>
+                            </div>
+                            <div class="mt-4">
+                                <a href="" class="btn-read-more bn632-hover bn20">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- about section end -->
+
+    <!-- latest project section -->
+
+        <div class="latest-projects-section">
+            <div class="gray-box-1"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-12">
+                        <h3 class="main-head mt-md-5 mt-3">Our Latest Projects</h3>
+                        <p class="main-para mt-md-5 mt-3">Lorem ipsum dolor sit amet consectetur. Dictum justo faucibus pulvinar pulvinar purus donec id tristique.
+                        Vulputate amet arcu sed in facilisis. Lorem ipsum dolor sit amet consectetur. Dictum justo faucibus pulvinar
+                        pulvinar</p>
+                    </div>
+                    <div class="col-md-8 col-12"></div>
+                </div>
+            </div>
+        </div>
+
+    
+    <!-- latest project section end -->
+
 
 
 
@@ -78,8 +131,25 @@
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- waypoint -->
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <!-- counter -->
+    <script src="assets/js/jquery.countup.js"></script>
     <!-- main js -->
     <script src="assets/js/main.js"></script>
+
+
+
+    <!-- counter -->
+    <script>
+        $('.counter').countUp(
+            {
+            delay: 5,
+            time: 1500
+            }
+        );
+    </script>
+    <!-- counter end -->
 </body>
 </html>
 
